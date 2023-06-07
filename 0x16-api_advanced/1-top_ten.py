efc25247012bcf8b5dcefc6cb41475d2de73513f
MundiaNderi <sarahngima77@gmail.com>
2023-06-07 10:51:30 +0300

    Documenting task 1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,18 +1,33 @@
 #!/usr/bin/python3
-# Module that prints top 10 titles
+"""Module for printing the top 10 titles of hot posts in a subreddit."""
+
 import requests
 
 
 def top_ten(subreddit):
-    '''Prints titles of top 10 hot posts for subreddit'''
+    '''Prints the titles of the top 10 hot posts for a subreddit.
+
+    Args:
+        subreddit (str): The name of the subreddit.
+
+    Returns:
+        None
+
+    Prints:
+        The titles of the top 10 hot posts for the subreddit.
+        Prints None if the subreddit is not found or an error occurs.
+    '''
     URL = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
-    USERAGENT = {'User-Agent':
-                 'Unix:com.alx.apiadvanced:task3 (by /u/_mundia_nderi_)'}
+    USERAGENT = {
+        'User-Agent': 'Unix:com.alx.apiadvanced:task1 (by /u/_mundia_nderi_)'}
     req = requests.get(URL, headers=USERAGENT)
-    if req.status_code is not 200:
+
+    if req.status_code != 200:
         print(None)
         return
+
     jreq = req.json()
     data_path = jreq['data']['children']
+
     for i in range(10):
         print(data_path[i]['data']['title'])
